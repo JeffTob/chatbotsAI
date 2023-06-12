a7c928eb6dbd6a7a310586d303dbb807ee8e97ed
JeffTob <jefersontob@gmail.com>
2023-06-12 14:59:07 -0700

    Updates

--- app.py
+++ app.py
@@ -152,7 +152,8 @@ def pageSupport2( api_key ):
       #llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003", openai_api_key=openai_api_key))
       llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="gpt-3.5-turbo", openai_api_key=openai_api_key))      
       prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-      index = GPTVectorStoreIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+      index = GPTVectorStoreIndex.from_documents(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+      
       #index = GPTVectorStoreIndex(documents)
       #response = index.query(query, text_qa_template=QA_PROMPT)
       response = index.query(query,response_mode="default")
