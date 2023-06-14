36b4bfce7042e286b2ec51f48a001993abb0525c
JeffTob <jefersontob@gmail.com>
2023-06-13 19:18:28 -0700

    Updates

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ from langchain.chat_models import ChatOpenAI
 def main():
     st.sidebar.title("Dashboard AI")  # Add title to the sidebar
 
-    st.title("Generative AI ChatBots")
-
+    st.title("Generative AI Application")
     
     openai_api_key = st.sidebar.text_input("Enter your OpenAI API key to start", type="password")  # Add OpenAI API Key input field    
 
@@ -34,12 +33,13 @@ def main():
     #page = st.sidebar.selectbox("Select a page", ["Chat Support-MLS Import Wizard", "Chat Support-MLS Import Wizard", "Chat PDF", "Chat MLS"])    
     page = st.sidebar.selectbox("Select a page", ["Chat Support", "Chat PDF", "Chat MLS"]) 
 
-    st.sidebar.text("The purpose of this Dashboard is to")
-    st.sidebar.text("learn, explore various ways on how to")
-    st.sidebar.text("use AI technology to solve various")
-    st.sidebar.text("problems also to enhance the effectiveness") 
-    st.sidebar.text("of our Products from Chatbots,")
-    st.sidebar.text("Data Visualization to Data Analysis.")  # Add title to the sidebar   
+    with st.sidebar:
+        st.markdown(
+                "This tool is a work in progress. "
+                "The purpose of this Dashboard is to learn, explore various ways on how to use AI technology to solve various problems also to enhance the effectiveness " 
+                "of our Products from Chatbots, Data Visualization to Data Analysis. "
+                "You can contribute to the project with your feedback and suggestions💡"
+            )
 
     st.sidebar.title("Bradford Technologies")  # Add title to the sidebar    
     st.sidebar.text("Developed by Jeferson Tobias")  # Add title to the sidebar    
@@ -121,196 +121,196 @@ def pageSupport( api_key ):
                 st.warning("Please load the URL content first.")
 
 def pageSupport2( api_key ):
-    
-    if api_key is None or api_key == "":
-        st.error("OPENAI_API_KEY is not set")
-        return
+    try:
+        if api_key is None or api_key == "":
+            st.error("OPENAI_API_KEY is not set")
+            return
 
-    st.title("💬 Chat - MLS Import Wizard")
-    st.write("Using: Web Scraper - BeautifulSoup,  LlamaIndex VectorStoreIndex documents, OpeanAI-GPT-3.5-turbo")
+        st.title("💬 Chat - MLS Import Wizard")
+        st.write("Using: Web Scraper - BeautifulSoup,  LlamaIndex VectorStoreIndex documents, OpeanAI-GPT-3.5-turbo")
         
-    def createDataSet(url):
-        print(url)
-        openai_api_key = api_key
+        def createDataSet(url):
+            print(url)
+            openai_api_key = api_key
 
-        BeautifulSoupWebReader = download_loader("BeautifulSoupWebReader")
-        loader = BeautifulSoupWebReader()
-        documents = loader.load_data(urls=[url])
-        # set maximum input size
-        max_input_size = 4096
-        # set number of output tokens
-        num_outputs = 2000
-        # set maximum chunk overlap
-        max_chunk_overlap = 0.2
-        # set chunk size limit
-        chunk_size_limit = 600 
+            BeautifulSoupWebReader = download_loader("BeautifulSoupWebReader")
+            loader = BeautifulSoupWebReader()
+            documents = loader.load_data(urls=[url])
+            # set maximum input size
+            max_input_size = 4096
+            # set number of output tokens
+            num_outputs = 2000
+            # set maximum chunk overlap
+            max_chunk_overlap = 0.2
+            # set chunk size limit
+            chunk_size_limit = 600 
 
-        os.environ["OPENAI_API_KEY"] = openai_api_key      
-        #llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="gpt-3.5-turbo", openai_api_key=openai_api_key))   
-        llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", openai_api_key=openai_api_key))     
-        prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit) 
-        serviceContext = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-        index_page = GPTVectorStoreIndex.from_documents(documents, serviceContext=serviceContext)  
-        index_page.storage_context.persist(persist_dir="data")
-        return index_page
+            os.environ["OPENAI_API_KEY"] = openai_api_key      
+            #llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="gpt-3.5-turbo", openai_api_key=openai_api_key))   
+            llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", openai_api_key=openai_api_key))     
+            prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit) 
+            serviceContext = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+            index_page = GPTVectorStoreIndex.from_documents(documents, serviceContext=serviceContext)  
+            index_page.storage_context.persist(persist_dir="data")
+            return index_page
 
-    #def getData(query, url):    
-    def getData(query):
-         
-      #openai_api_key = api_key
-      '''
-      BeautifulSoupWebReader = download_loader("BeautifulSoupWebReader")
-      loader = BeautifulSoupWebReader()
-      documents = loader.load_data(urls=[url])
-      '''  
-      QA_PROMPT_TMPL = (
-            "We have provided context information below.\n"
-            "---------------------\n"
-            "{context_str}"
-            "\n---------------------\n"
-            #"Given this information, please answer the question in bullet point format with a new line after each point and cross reference any data cited in the document.\n"
-            "warn the user if any information seems off: {query_str}\n"
-        )
-      QA_PROMPT = QuestionAnswerPrompt(QA_PROMPT_TMPL)
-      
-      #response = index.query(query, text_qa_template=QA_PROMPT)
-      #response = index.query(query,response_mode="default")
-      storage_context = StorageContext.from_defaults(persist_dir="data")
-      index = load_index_from_storage(storage_context)
-      query_engine = index.as_query_engine()
-      response = query_engine.query(query)
-      return response
+        #def getData(query, url):    
+        def getData(query):         
+        
+            QA_PROMPT_TMPL = (
+                    "We have provided context information below.\n"
+                    "---------------------\n"
+                    "{context_str}"
+                    "\n---------------------\n"
+                    #"Given this information, please answer the question in bullet point format with a new line after each point and cross reference any data cited in the document.\n"
+                    "warn the user if any information seems off: {query_str}\n"
+                )
+            QA_PROMPT = QuestionAnswerPrompt(QA_PROMPT_TMPL)
+        
+            #response = index.query(query, text_qa_template=QA_PROMPT)      
+            storage_context = StorageContext.from_defaults(persist_dir="data")
+            index = load_index_from_storage(storage_context)
+            query_engine = index.as_query_engine()
+            response = query_engine.query(query)
+            return response
 
-    #url_input = st.text_input("Enter a URL")
-    default_url = "https://support.bradfordsoftware.com/techtips/cf-mls-import-wizard.html"
-    with st.form("url_input_form"):
-      url_input = st.text_input("Enter URL:", value=default_url, help="Enter the URL of a web page")
-      load_button = st.form_submit_button("Load Data")    
+        #url_input = st.text_input("Enter a URL")
+        default_url = "https://support.bradfordsoftware.com/techtips/cf-mls-import-wizard.html"
+        with st.form("url_input_form"):
+            #url_input = st.text_input("Enter URL:", value=default_url, help="Enter the URL of a web page")
+            st.code(default_url, language="markdown")
+            load_button = st.form_submit_button("Create Dataset")       
 
-    with st.form("chat_input", clear_on_submit=True):
-        a, b = st.columns([4, 1])
-        user_input = a.text_input(
-            label="Your message:",
-            placeholder="What would you like to ask about the URL content?",
-            label_visibility="collapsed",
-        )
-        send_button = b.form_submit_button("Send", use_container_width=True)
+        with st.form("chat_input", clear_on_submit=True):
+            a, b = st.columns([4, 1])
+            user_input = a.text_input(
+                label="Your message:",
+                placeholder="What would you like to ask about the URL content?",
+                label_visibility="collapsed",
+            )
+            send_button = b.form_submit_button("Send", use_container_width=True)
 
-    if load_button:
-        createDataSet(url_input)
-        message('How can I help you ?', is_user=False)  # Display AI's response    
+        if load_button:
+            createDataSet(default_url)
+            message('How can I assist you ?', is_user=False)  # Display AI's response    
 
-    #if st.button("Send"):
-    if send_button:
-      with st.spinner(text="Thinking..."):
-        #response = getData(query=user_input, url=url_input)
-        response = getData(query=user_input)
-        response_text = str(response)  # Convert the response to a string
-        message(user_input, is_user=True)
-        message(response_text, is_user=False)  # Display AI's response
+        #if st.button("Send"):
+        if send_button:
+            with st.spinner(text="Thinking..."):
+                #response = getData(query=user_input, url=url_input)
+                response = getData(query=user_input)
+                response_text = str(response)  # Convert the response to a string
+                message(user_input, is_user=True)
+                message(response_text, is_user=False)  # Display AI's response
+
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
 
 def pagePDF( api_key ):
-    
-    if api_key is None or api_key == "":
-        st.error("OPENAI_API_KEY is not set")
-        return
-    
-    st.title("💬 Chat PDF")
-    st.write("LangChain embeddings")
+    try:
+        if api_key is None or api_key == "":
+            st.error("OPENAI_API_KEY is not set")
+            return
+        
+        st.title("💬 Chat PDF")
+        st.write("LangChain embeddings")
 
-    # upload file
-    pdf = st.file_uploader("Upload your PDF", type="pdf")
+        # upload file
+        pdf = st.file_uploader("Upload your PDF", type="pdf")
 
-    # extract the text
-    if pdf is not None:
-        pdf_reader = PdfReader(pdf)
-        text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
+        # extract the text
+        if pdf is not None:
+            pdf_reader = PdfReader(pdf)
+            text = ""
+            for page in pdf_reader.pages:
+                text += page.extract_text()
 
-        # split into chunks
-        text_splitter = CharacterTextSplitter(
-            separator="\n",
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len
-        )
-        chunks = text_splitter.split_text(text)
-
-        # create embeddings
-        #embeddings = OpenAIEmbeddings()
-        embeddings = OpenAIEmbeddings(openai_api_key=api_key)
-        knowledge_base = FAISS.from_texts(chunks, embeddings)
-
-        # show user input
-        with st.form("pdf_input_form", clear_on_submit=True):
-            a, b = st.columns([4, 1])
-            user_question = a.text_input(
-                label="Ask a question about your PDF:",
-                placeholder="What would you like to ask about your PDF ?",
-                label_visibility="collapsed",
+            # split into chunks
+            text_splitter = CharacterTextSplitter(
+                separator="\n",
+                chunk_size=1000,
+                chunk_overlap=200,
+                length_function=len
             )
-            b.form_submit_button("Send", use_container_width=True)
+            chunks = text_splitter.split_text(text)
 
-        # show user input
-        #user_question = st.text_input("Ask a question about your PDF:")
-        if user_question:
-            with st.spinner(text="Thinking..."):
-                docs = knowledge_base.similarity_search(user_question)
+            # create embeddings
+            #embeddings = OpenAIEmbeddings()
+            embeddings = OpenAIEmbeddings(openai_api_key=api_key)
+            knowledge_base = FAISS.from_texts(chunks, embeddings)       
 
-                llm = OpenAI(openai_api_key=api_key)
-                chain = load_qa_chain(llm, chain_type="stuff")
-                with get_openai_callback() as cb:
-                    response = chain.run(input_documents=docs, question=user_question)
-                    #response_text = str(response)  # Convert the response to a string
-                    message(user_question, is_user=True)
-                    message(response, is_user=False)  # Display AI's response
-                    #print(cb)
-                #st.write(response)               
+            # show user input
+            with st.form("pdf_input_form", clear_on_submit=True):
+                a, b = st.columns([4, 1])
+                user_question = a.text_input(
+                    label="Ask a question about your PDF:",
+                    placeholder="What would you like to ask about your PDF ?",
+                    label_visibility="collapsed",
+                )
+                b.form_submit_button("Send", use_container_width=True)
 
+            # show user input
+            #user_question = st.text_input("Ask a question about your PDF:")
+            #message('How can I assist you ?', is_user=False) 
+            if user_question:
+                with st.spinner(text="Thinking..."):
+                    docs = knowledge_base.similarity_search(user_question)
+
+                    llm = OpenAI(openai_api_key=api_key)
+                    chain = load_qa_chain(llm, chain_type="stuff")
+                    with get_openai_callback() as cb:
+                        response = chain.run(input_documents=docs, question=user_question)
+                        #response_text = str(response)  # Convert the response to a string
+                        message(user_question, is_user=True)
+                        message(response, is_user=False)  # Display AI's response
+                        #print(cb)
+                    #st.write(response)               
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
 
 def pageMLS( api_key ):
-    
-    if api_key is None or api_key == "":
-        st.error("OPENAI_API_KEY is not set")
-        return
-    
-    st.title("💬 Chat MLS ")
-    st.write("LangChain Agent")
-
-    csv_file = st.file_uploader("Upload a CSV file", type="csv")
-    #csv_path = csv_file.name
-    if csv_file is not None:
-        csv_path = os.path.join(os.getcwd(), csv_file.name)  # Get the path of the uploaded CSV file
-        with open(csv_path, "wb") as f:
-            f.write(csv_file.read())  # Save the uploaded CSV file to the specified path
-        agent = create_csv_agent(OpenAI(openai_api_key=api_key, temperature=0), csv_path, verbose=True)
-        os.remove(csv_path)
-        #agent = create_csv_agent(OpenAI(openai_api_key=api_key, temperature=0), csv_file, verbose=True)        
-        #agent = create_csv_agent(OpenAI(openai_api_key=api_key, temperature=0), csv_file, verbose=True)
+    try:
+        if api_key is None or api_key == "":
+            st.error("OPENAI_API_KEY is not set")
+            return
         
+        st.title("💬 Chat MLS ")
+        st.write("LangChain Agent")
 
-        #user_question = st.text_input("Ask a question about your CSV: ")
-        # show user input
-        with st.form("csv_input_form", clear_on_submit=True):
-            a, b = st.columns([4, 1])
-            user_question = a.text_input(
-                label="Ask a question about your MLS:",
-                placeholder="What would you like to ask about your MLS ?",
-                label_visibility="collapsed",
-            )
-            b.form_submit_button("Send", use_container_width=True)
+        csv_file = st.file_uploader("Upload a CSV file", type="csv")
+        #csv_path = csv_file.name
+        if csv_file is not None:
+            csv_path = os.path.join(os.getcwd(), csv_file.name)  # Get the path of the uploaded CSV file
+            with open(csv_path, "wb") as f:
+                f.write(csv_file.read())  # Save the uploaded CSV file to the specified path
+            agent = create_csv_agent(OpenAI(openai_api_key=api_key, temperature=0), csv_path, verbose=True)
+            os.remove(csv_path)
+            #agent = create_csv_agent(OpenAI(openai_api_key=api_key, temperature=0), csv_file, verbose=True)        
+            #agent = create_csv_agent(OpenAI(openai_api_key=api_key, temperature=0), csv_file, verbose=True)       
 
-        if user_question is not None and user_question != "":
-            with st.spinner(text="Thinking..."):
-                prompt = user_question[:4000]  # Truncate the prompt to 4000 characters
-                completion_length = 700  # Set the desired completion length
-                completion = agent.run(prompt)[:completion_length]  # Truncate the completion to the desired length
-                with st.spinner(text="In progress..."):                    
-                    message(prompt, is_user=True)
-                    message(completion, is_user=False)  # Display AI's response
-                #st.write(completion)
-
+            #user_question = st.text_input("Ask a question about your CSV: ")
+            # show user input
+            with st.form("csv_input_form", clear_on_submit=True):
+                a, b = st.columns([4, 1])
+                user_question = a.text_input(
+                    label="Ask a question about your MLS:",
+                    placeholder="What would you like to ask about your MLS ?",
+                    label_visibility="collapsed",
+                )
+                b.form_submit_button("Send", use_container_width=True)
+            
+            #message('How can I assist you ?', is_user=False)  # Display AI's response 
+            if user_question is not None and user_question != "":
+                with st.spinner(text="Thinking..."):
+                    prompt = user_question[:4000]  # Truncate the prompt to 4000 characters
+                    completion_length = 700  # Set the desired completion length
+                    completion = agent.run(prompt)[:completion_length]  # Truncate the completion to the desired length
+                    with st.spinner(text="In progress..."):                    
+                        message(prompt, is_user=True)
+                        message(completion, is_user=False)  # Display AI's response
+                    #st.write(completion)
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
